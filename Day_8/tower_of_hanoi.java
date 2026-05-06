4c9254466d5351424db861a7616d25822aa72c22
Chinmaya Biswal <chinmayabiswal777@gmail.com>
2026-05-06 12:53:46 +0530

    Day 8 codes

---
+++ Day_8/tower_of_hanoi.java
@@ -0,0 +1,18 @@
+package Day_8;
+
+public class tower_of_hanoi {
+    public static void tower(int n,String src,String helper,String dest){
+       if(n==1){
+         System.out.println("Transfer disk "+n+" from"+src+"to"+dest);
+         return;
+}
+        tower(n-1,src,dest,helper);
+        System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
+        tower(n-1,helper,src,dest);
+    }
+    public static void main (String args []){
+       int n = 3; 
+       tower(n,"s","h","d");
+}
+}
+
